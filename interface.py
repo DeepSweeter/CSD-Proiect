43b229e663ad2a839464e11407ad10719665182c
Ionut-Andrei_Poclid <ionut-andrei.poclid@student.tuiasi.ro>
2024-05-11 21:44:01 +0300

    Change interface aspect and add dynamic files updater

--- interface.py
+++ interface.py
@@ -5,10 +5,19 @@ import os
 from rc6 import *
 import socket
 from connection import connection_handler
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 dir_path = "./Received Data"
 file_names = [i for i in os.listdir(dir_path) if i.endswith('.txt')]
 file_names.insert(0, "None")
+
+class Updater(FileSystemEventHandler):
+    def __init__(self, app):
+        self.app = app
+
+    def on_modified(self, event):
+        self.app.update_files()
 
 class mainPanel:
     def __init__(self) -> None:
@@ -29,15 +38,15 @@ class mainPanel:
                                font=("Times New Roman", 11, "bold"), relief="groove")
         self.openButton.place(x=450, y=20, width=95, height=45)
 
-        # Button encrypt
-        self.cryptButton = tk.Button(self.window, text="Encrypt ->", command=self.bencrypt, background="white", foreground="darkblue",
-                                font=("Times New Roman", 11, "bold"), relief="groove")
-        self.cryptButton.place(x=450, y=90, width=95, height=45)
+        # Button encrypt (sters)
+        # self.cryptButton = tk.Button(self.window, text="Encrypt ->", command=self.bencrypt, background="white", foreground="darkblue",
+        #                         font=("Times New Roman", 11, "bold"), relief="groove")
+        # self.cryptButton.place(x=450, y=90, width=95, height=45)
 
-        # Button decrypt
-        self.decryptButton = tk.Button(self.window, text="Decrypt ->", command=self.bdecrypt, background="white", foreground="darkblue",
-                                  font=("Times New Roman", 11, "bold"), relief="groove")
-        self.decryptButton.place(x=450, y=250, width=95, height=45)
+        # Button decrypt (sters)
+        # self.decryptButton = tk.Button(self.window, text="Decrypt ->", command=self.bdecrypt, background="white", foreground="darkblue",
+        #                           font=("Times New Roman", 11, "bold"), relief="groove")
+        # self.decryptButton.place(x=450, y=250, width=95, height=45)
 
         # Button send packet
         self.sendButton = tk.Button(self.window, text="Send data", background="red", foreground="white", command = self.sendFileAction,
@@ -62,13 +71,13 @@ class mainPanel:
         self.label2 = tk.Label(self.window, text="Cypher text:", font=("Arial Black", 8))
         self.label2.place(x=580, y=0)
         self.text_box_cypher_text = tk.Text(self.window, font=("Times New Roman", 14), foreground="darkred", relief="solid")
-        self.text_box_cypher_text.place(x=580, y=20, width=400, height=200)
+        self.text_box_cypher_text.place(x=580, y=20, width=400, height=430)
 
-        # Textbox decrypt text
-        self.label3 = tk.Label(self.window, text="Decrypt text:", font=("Arial Black", 8))
-        self.label3.place(x=580, y=230)
-        self.text_box_decrypt_text = tk.Text(self.window, font=("Times New Roman", 14), foreground="darkgreen", relief="solid")
-        self.text_box_decrypt_text.place(x=580, y=250, width=400, height=200)
+        # Textbox decrypt text (sters)
+        # self.label3 = tk.Label(self.window, text="Decrypt text:", font=("Arial Black", 8))
+        # self.label3.place(x=580, y=230)
+        # self.text_box_decrypt_text = tk.Text(self.window, font=("Times New Roman", 14), foreground="darkgreen", relief="solid")
+        # self.text_box_decrypt_text.place(x=580, y=250, width=400, height=200)
 
         # Textbox key
         self.label4 = tk.Label(self.window, text="Key:", font=("Arial Black", 8))
@@ -94,6 +103,10 @@ class mainPanel:
         self.comboboxRM.current(0)
         self.comboboxRM.bind("<<ComboboxSelected>>", self.update_textBoxCT)
         self.comboboxRM.place(x=440, y=175, width=115, height=30)
+        objUp = Updater(self)
+        observer = Observer()
+        observer.schedule(objUp, path=dir_path, recursive=False)
+        observer.start()
 
         # My IP
         ip = socket.gethostbyname(socket.gethostname())
@@ -126,59 +139,59 @@ class mainPanel:
             self.text_box_plain_text.insert(tk.END, text)
 
 
-    def bencrypt(self):
-        ctext = ''
-        key = self.text_key_text.get("1.0", tk.END)
-        key = bytes.fromhex(key.replace(" ", ""))
-        obj = rc6(key)
+    # def bencrypt(self):  (sters)
+    #     ctext = ''
+    #     key = self.text_key_text.get("1.0", tk.END)
+    #     key = bytes.fromhex(key.replace(" ", ""))
+    #     obj = rc6(key)
+    #
+    #     p_text = self.text_box_plain_text.get("1.0", tk.END)
+    #     p_text = bytes.fromhex(p_text.replace(" ", ""))
+    #     ctext = obj.encrypt(p_text)
+    #
+    #     x = ' '.join([f'{i:02x}' for i in ctext])
+    #
+    #     self.text_box_cypher_text.delete("1.0", tk.END)
+    #     self.text_box_cypher_text.insert(tk.END, x)
 
-        p_text = self.text_box_plain_text.get("1.0", tk.END)
-        p_text = bytes.fromhex(p_text.replace(" ", ""))
-        ctext = obj.encrypt(p_text)
+    # def bdecrypt(self):  (sters)
+    #     dtext = ''
+    #     key = self.text_key_text.get("1.0", tk.END)
+    #     key = bytes.fromhex(key.replace(" ", ""))
+    #     obj = rc6(key)
+    #
+    #     c_text = self.text_box_cypher_text.get("1.0", tk.END)
+    #     c_text = bytes.fromhex(c_text.replace(" ", ""))
+    #     dtext = obj.decrypt(c_text)
+    #
+    #     x = ' '.join([f'{i:02x}' for i in dtext])
+    #
+    #     self.text_box_decrypt_text.delete("1.0", tk.END)
+    #     self.text_box_decrypt_text.insert(tk.END, x)
 
-        x = ' '.join([f'{i:02x}' for i in ctext])
-
-        self.text_box_cypher_text.delete("1.0", tk.END)
-        self.text_box_cypher_text.insert(tk.END, x)
-
-    def bdecrypt(self):
-        dtext = ''
-        key = self.text_key_text.get("1.0", tk.END)
-        key = bytes.fromhex(key.replace(" ", ""))
-        obj = rc6(key)
-
-        c_text = self.text_box_cypher_text.get("1.0", tk.END)
-        c_text = bytes.fromhex(c_text.replace(" ", ""))
-        dtext = obj.decrypt(c_text)
-
-        x = ' '.join([f'{i:02x}' for i in dtext])
-
-        self.text_box_decrypt_text.delete("1.0", tk.END)
-        self.text_box_decrypt_text.insert(tk.END, x)
+    def update_files(self):
+        file_names = [i for i in os.listdir(dir_path) if i.endswith('.txt')]
+        file_names.insert(0, "None")
+        self.comboboxRM['values'] = file_names
 
     def update_textBoxCT(self, event):
         selected_file = self.comboboxRM.get()
 
         if selected_file != 'None':
             with open(os.path.join(dir_path, selected_file), 'r') as f:
-                x = f.readlines()
+                cypherText = f.read()
 
-            cypherText = x[0].strip("\n")
-            key = x[1].strip("\n")
         else:
             self.text_box_cypher_text.delete('1.0', tk.END)
-            self.text_key_text.delete('1.0', tk.END)
             return
 
         self.text_box_cypher_text.delete('1.0', tk.END)
         self.text_box_cypher_text.insert(tk.END, cypherText)
-        self.text_key_text.delete('1.0', tk.END)
-        self.text_key_text.insert(tk.END, key)
 
     def clearAll(self):
         self.text_box_plain_text.delete("1.0", tk.END)
         self.text_box_cypher_text.delete("1.0", tk.END)
-        self.text_box_decrypt_text.delete("1.0", tk.END)
+        #self.text_box_decrypt_text.delete("1.0", tk.END)
         self.text_key_text.delete("1.0", tk.END)
         return
     
